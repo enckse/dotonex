372143c132e97817414f972f5c373c1342d56743
Sean Enck <enckse@gmail.com>
2018-04-19 12:10:02 -0400

    whoops, forgot new test user

---
+++ tools/users/user_admin.py
@@ -0,0 +1,7 @@
+"""User with attributes."""
+import users.__config__ as __config__
+normal = __config__.Assignment()
+normal.macs = ["001122334455"]
+normal.vlan = "dev"
+normal.administrator = True
+normal.password = 'e08bb5212dd623b9a1cd4d25bcc8b89r'
