365631a6f8b10d89d8dca2b196080699a92a5cfd
Sean Enck <enckse@gmail.com>
2018-06-08 09:12:40 -0400

    Revert "var for report name"
    This reverts commit f0237ff93e3a324eb3be58417061de1ee5111557.

--- tools/reports.sh
+++ tools/reports.sh
@@ -19,10 +19,9 @@ _post() {
     local d
     d=$(date +%Y-%m-%d)
     for f in $(ls $BIN | grep "\.md"); do
-        fname=$BIN/$f
-        echo "" >> $fname
-        echo "generated on: $d" >> $fname
-        content=$(cat $fname | python -c "import sys, urllib.parse; print(urllib.parse.quote(sys.stdin.read()))")
+        echo "" >> $BIN/$f
+        echo "generated on: $d" >> $BIN/$f
+        content=$(cat $BIN/$f | python -c "import sys, urllib.parse; print(urllib.parse.quote(sys.stdin.read()))")
         name=$(echo "$f" | cut -d "." -f 1)
         curl -s -k -X POST -d "name=$name&content=$content" "$RPT_HOST/reports/upload?session=$RPT_TOKEN"
     done
