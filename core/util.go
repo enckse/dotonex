46058bce57e392510fc63b7d87a78610d74468a5
Sean Enck <enckse@voidedtech.com>
2019-07-17 21:17:46 -0400

    working on a common keyvalue store

--- core/util.go
+++ core/util.go
@@ -1,8 +1,32 @@
 package core
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 )
+
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+type KeyValueStore struct {
+	KeyValues []KeyValue
+}
+
+func (kv KeyValueStore) String() string {
+	var b bytes.Buffer
+	offset := ""
+	for idx, k := range kv.KeyValues {
+		if idx > 0 {
+			b.Write([]byte("\n"))
+		}
+		b.Write([]byte(fmt.Sprintf("%s%s = %s", offset, k.Key, k.Value)))
+		offset = "  "
+	}
+	return b.String()
+}
 
 func PathExists(file string) bool {
 	if _, err := os.Stat(file); os.IsNotExist(err) {
