c9054bb819dc565866e82c6737fbe344f676ee8a
Sean Enck <enckse@voidedtech.com>
2019-12-17 13:27:09 -0500

    configure logging happens 2 times now but the first time is protective

--- cmd/radiucal-runner/main.go
+++ cmd/radiucal-runner/main.go
@@ -143,8 +143,9 @@ func account(ctx *server.Context) {
 }
 
 func main() {
-	core.Version(vers)
 	p := core.Flags()
+	core.ConfigureLogging(p.Debug, p.Instance)
+	core.WriteInfo(vers)
 	b, err := ioutil.ReadFile(p.Config)
 	if err != nil {
 		core.Fatal("unable to load config", err)
