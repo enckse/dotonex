67f2f17c6b6f53f4db6a23a823f996e24f410540
Sean Enck <enckse@voidedtech.com>
2020-07-31 21:49:08 -0400

    renaming this

--- internal/management/objects.go
+++ internal/management/objects.go
@@ -1,4 +1,4 @@
-package authem
+package management
 
 import (
 	"fmt"
@@ -40,7 +40,7 @@ type (
 		Net         string
 	}
 
-	// UserPermissions reflect controlled permissions for a user within all of authem
+	// UserPermissions reflect controlled permissions for a user within all of management
 	UserPermissions struct {
 		IsRADIUS bool
 		IsPEAP   bool
@@ -119,7 +119,7 @@ func (u *User) Inflate(key string, secrets []*Secret) error {
 	return fmt.Errorf("user secrets not configured")
 }
 
-// GetKey retrieves the authem key used for secrets
+// GetKey retrieves the management key used for secrets
 func GetKey(optional bool) (string, error) {
 	k := os.Getenv("AUTHEM_KEY")
 	if strings.TrimSpace(k) == "" {
