802eb4c178b6f25699dea6c6f8066d19d6c8c6a6
Sean Enck <enckse@voidedtech.com>
2021-01-05 15:33:43 -0500

    Revert "deprecated"
    This reverts commit 8046afa8c67e2dd315e6a2c0db56f0eb94f68645.

---
+++ internal/core/encrypt_test.go
@@ -0,0 +1,37 @@
+package core
+
+import (
+	"testing"
+)
+
+func TestEncryptDecrypt(t *testing.T) {
+	key := "aaaaaaaabbbbbbbbccccccccdddddddd"
+	enc, err := Encrypt(key, "secret")
+	if err != nil {
+		t.Errorf("invalid request %v", err)
+	}
+	if enc == "" {
+		t.Error("invalid encryption string", enc)
+	}
+	dec, err := Decrypt(key, enc)
+	if err != nil {
+		t.Errorf("invalid request %v", err)
+	}
+	if dec != "secret" {
+		t.Error("decryption error", dec)
+	}
+	_, err = Decrypt("garbage", enc)
+	if err == nil {
+		t.Error("invalid request")
+	}
+	_, err = Encrypt("garbage", enc)
+	if err == nil {
+		t.Error("invalid request")
+	}
+}
+
+func TestMD4(t *testing.T) {
+	if o := MD4("test"); o != "0cb6948805f797bf2a82807973b89537" {
+		t.Error("invalid md4")
+	}
+}
