fa6c01d1d134da37e25331f776ebf9c61d16f6ec
Sean Enck <enckse@voidedtech.com>
2021-01-06 09:33:14 -0500

    these are wrappers at a lower level that are being rewritten

--- configs/scripts/lib/samba.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-{{ range $key, $user := .Users }}
-if pdbedit -L | grep -q "^{{ $user.LoginName }}:"; then
-    echo "samba passwd update: {{ $user.LoginName }}"
-    printf "%s\n%s\n" {{ $user.Password }} {{ $user.Password }} | smbpasswd -s {{ $user.LoginName }}
-fi
-{{ end }}
