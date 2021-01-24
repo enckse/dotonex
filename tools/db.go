8c5911e7f730bc4d24fa8b39dc15c03cc275b93f
Sean Enck <enckse@voidedtech.com>
2021-01-23 21:54:58 -0500

    new tool

---
+++ tools/db.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/tidwall/buntdb"
+)
+
+func main() {
+	file := flag.String("database", "", "database to read")
+	flag.Parse()
+	db, err := buntdb.Open(*file)
+	if err != nil {
+		panic(fmt.Sprintf("failed to open (%v)", err))
+	}
+	err = db.View(func(tx *buntdb.Tx) error {
+		err := tx.Ascend("", func(key, value string) bool {
+			fmt.Printf("%s:::%s\n", key, value)
+			return true
+		})
+		return err
+	})
+}
